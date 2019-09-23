1df5915c3d358eb4d55736974b435368a8749af9
Brian Funk <brfunk@indiana.edu>
2019-09-23 11:45:23 -0400

    Updating to be better v2 -remove import

--- reportLabBreakdown.py
+++ reportLabBreakdown.py
@@ -11,7 +11,7 @@ from PyQt5.QtGui import QFont, QIcon
 from PyQt5 import QtCore
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import htmlbase
 
 iconPath = functions.createPath('PIEcon.png')
