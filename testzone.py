66cc56f81c03daa3e572eb4687ef3f8d279568af
Brian Funk <brfunk@indiana.edu>
2019-10-07 11:16:39 -0400

    remove dependency check lol

--- testzone.py
+++ testzone.py
@@ -1,21 +1,5 @@
 import sys
 import loginGui
-import subprocess
-import pkg_resources
-
-modules = [p.project_name for p in pkg_resources.working_set]
-print(modules)
-
-def install(package):
-    subprocess.call([sys.executable, "-m", "pip", "install", package])
-
-dependencies = ['PyQt5', 'selenium', 'requests', 'pandas', 'numpy']
-for package in dependencies:
-    if package not in modules:
-        print('installing ' + package)
-        install(package)
-
-
 from PyQt5.QtWidgets import (QApplication)
 
 if __name__ == '__main__':
